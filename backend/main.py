ef665d69ab773aae485abd2c68a20c16750182c1
Ray Gough <rayjgough1@gmail.com>
2026-06-27 23:37:16 -0400

    FastAPI server up and running

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 import argparse, time
+import random
 from datetime import datetime
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from quiz import Word, Quiz, Session
 from quiz import valid_rounds
 
@@ -7,34 +10,45 @@ from quiz import valid_rounds
 '''
 Promgram Driver
 '''
+app = FastAPI()
 
-def Main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--category", type=str, help="category of word",
-                        choices=["nouns", "verbs", "adjectives"], required=True)
+@app.get("/")
+def home():
+    return {"message": "Willkomen meinen menschen!"}
+
+@app.get("/word")
+def get_word(category: str):
+    word = Word(category)
+    word.make_word()
+    return {"word": word.word, "meaning": word.meaning}
+
+# def Main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("-c", "--category", type=str, help="category of word",
+#                         choices=["nouns", "verbs", "adjectives"], required=True)
     
-    parser.add_argument("-r", "--rounds", type=valid_rounds, help="number of questions", required=True)
-    args = parser.parse_args()
+#     parser.add_argument("-r", "--rounds", type=valid_rounds, help="number of questions", required=True)
+#     args = parser.parse_args()
 
-    user_cat = args.category
-    user_rounds = args.rounds
+#     user_cat = args.category
+#     user_rounds = args.rounds
     
-    quiz = Quiz()
-    print("You can press \"x\" at any point to exit the session\n")
+#     quiz = Quiz()
+#     print("You can press \"x\" at any point to exit the session\n")
 
-    start = time.time()
-    for i in range(user_rounds):
-        word = Word(input=user_cat)
-        word.make_word()
-        if not quiz.test(word.word, word.meaning):
-            break
-    end = time.time()
+#     start = time.time()
+#     for i in range(user_rounds):
+#         word = Word(input=user_cat)
+#         word.make_word()
+#         if not quiz.test(word.word, word.meaning):
+#             break
+#     end = time.time()
     
-    rounds_done = user_rounds if user_rounds == quiz.num_rounds else quiz.num_rounds
-    session = Session(n_questions=rounds_done, category= user_cat,
-                      session_length= (end - start), session_occurance=datetime.now(),
-                       correct= quiz.correct)
-    session.save()
-    session.summary()
+#     rounds_done = user_rounds if user_rounds == quiz.num_rounds else quiz.num_rounds
+#     session = Session(n_questions=rounds_done, category= user_cat,
+#                       session_length= (end - start), session_occurance=datetime.now(),
+#                        correct= quiz.correct)
+#     session.save()
+#     session.summary()
 
-Main()
+# Main()
