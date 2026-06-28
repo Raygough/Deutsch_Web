2549103b599b32523eee5c69c1914f5d0e625b8a
Ray Gough <rayjgough1@gmail.com>
2026-06-27 22:04:07 -0400

    Final CLI changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse, time
+from datetime import datetime
 from quiz import Word, Quiz, Session
 from quiz import valid_rounds
 
@@ -7,26 +8,7 @@ from quiz import valid_rounds
 Promgram Driver
 '''
 
-def time_cal(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        user_func = func(*args, **kwargs)
-        end = time.time()
-
-        print(f"your total session time " \
-              f"was {end - start:.2f} seconds")
-        return user_func
-
-    return wrapper
-
-@time_cal
 def Main():
-    print('''
-        Welcome to the Detusch CLI Tool!\n
-        Please follow the prompts to use the tool.\n
-        Please type one of the following categories: nouns, verbs, adjectives
-          ''')
-
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--category", type=str, help="category of word",
                         choices=["nouns", "verbs", "adjectives"], required=True)
@@ -40,14 +22,19 @@ def Main():
     quiz = Quiz()
     print("You can press \"x\" at any point to exit the session\n")
 
+    start = time.time()
     for i in range(user_rounds):
         word = Word(input=user_cat)
         word.make_word()
         if not quiz.test(word.word, word.meaning):
             break
+    end = time.time()
     
     rounds_done = user_rounds if user_rounds == quiz.num_rounds else quiz.num_rounds
-    session = Session(rounds_done, quiz.correct)
+    session = Session(n_questions=rounds_done, category= user_cat,
+                      session_length= (end - start), session_occurance=datetime.now(),
+                       correct= quiz.correct)
+    session.save()
     session.summary()
 
 Main()
--- quiz.py
+++ quiz.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, csv, os
 
 '''
 Logic for CLI Quiz Tool
@@ -81,17 +81,56 @@ class Quiz():
 class Session():
     def __init__(self,
                  n_questions,
+                 category,
+                 session_length,
+                 session_occurance,
                  correct):
+        '''
+        Args:
+        n_questions: int
+            number of questions the user answered
+        category: str
+            the category of words the user was prompted with
+        session_length: time
+            how long the session took
+        session_occurange: time
+            date of the session
+        correct: int
+            how many prompts the user answered correctly
+        '''
+
         self.n_questions = n_questions
+        self.category = category
+        self.session_length = session_length
+        self.session_occurance = session_occurance
         self.correct = correct
         self.accuracy =  self.correct / self.n_questions
 
 
     def summary(self):
-        print(f"Out of {self.n_questions} questions, you got {self.correct} correct!")
+        print(f"Out of {self.n_questions} questions in \"{self.category}\", you got {self.correct} correct!")
         if (self.accuracy == 1.00):
             print("That's an accuracy of 100%, Great Job!")
         else:
             print(f"That's an accuracy of {self.accuracy:.2f}%, Great Job!")
+
+        print(f"Your session took {self.session_length} seconds")
+    
+    def save(self):
+        with open('session.csv', 'a') as sessionData:
+            col_names = ['num_questions', 'category',
+            'session_length', 'session_date_time',
+            'num_correct', 'session_accuracy']
+            writer = csv.DictWriter(sessionData, fieldnames=col_names, delimiter= ',')
+
+            if not os.path.exists('session.csv') or os.path.getsize('session.csv') == 0:
+                writer.writeheader()
+            writer.writerow({
+                'num_questions': self.n_questions, 
+                'category': self.category,
+                'session_length': self.session_length,
+                'session_date_time': self.session_occurance,
+                'num_correct': self.correct, 
+                'session_accuracy': self.accuracy})
 
 
