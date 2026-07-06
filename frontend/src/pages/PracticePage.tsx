f3ea33ec4ebf3b6a2e98d1706f2d4b473fd73ced
Ray Gough <rayjgough1@gmail.com>
2026-07-05 20:17:03 -0400

    Updated Frontend

--- frontend/src/pages/PracticePage.tsx
+++ frontend/src/pages/PracticePage.tsx
@@ -37,9 +37,16 @@ function StartScreen({validate, setCategory, setRounds}: {
 }) {
   return (
     <>
-    <div className='p-10 align-center'>
-      <h1>Hey Ray, let's start a new session</h1>
-      <p>I want to practice nouns for 10 rounds</p>
+    <div className="bg-gray-500 w-100% h-10">
+      <nav>
+        <div>
+          <button className="hover:bg-gray-500" onClick={() => "/"}>Back</button>
+          <h3>Practice</h3>
+        </div>
+      </nav>
+    </div>
+    <div className='text-6xl p-10 align-center'>
+      <h1>Hey Ray, let's start a new session: </h1>
       <select onChange={(e) => setCategory(e.target.value)}
       name="categories" id="categories">
         <option value="nouns">Nouns</option>
@@ -48,7 +55,7 @@ function StartScreen({validate, setCategory, setRounds}: {
       </select>
       <input onChange={(e) => setRounds(parseInt(e.target.value))}
       type="text" placeholder='enter how many questions you want to answer'/>
-      <button onClick={() => validate()}>Continue</button>
+      <button className="p-10" onClick={() => validate()}>Continue</button>
     </div>
     </>
   )
@@ -58,12 +65,40 @@ function SessionScreen ({ word } :
   {word: {word: string, meaning: string,
     gender: string | null,
     category: string} | null}) {
+      const [input, setInput] = useState("")
+      const [valid, setValid] = useState("idle")
+
+      function checkValid() {
+        if(input.toLowerCase() == word?.meaning.toLowerCase()){
+          setValid("correct")
+        } else {
+          setValid("invalid")
+        }
+      }
+
+      let feedback = null
+      if(valid == "idle"){
+        feedback = null
+      } else if(valid == "correct"){
+        feedback = <h1>Richtig! {word?.word} means {input} </h1>
+      } else {
+        feedback = <h1>Falsch! {word?.word} does not mean {input}. It means
+         {word?.meaning}</h1>
+      }
+
   return (
+    <>
     <div className="bg-grey-500">
       <p>What does {word?.word} mean?</p>
-      <input type="text" />
+      <input onChange={(e) => setInput(e.target.value)} type="text" />
+      <button onClick={() => checkValid()}>Check</button>
     </div>
+    <div>
+      {feedback}
+    </div>
+    </>
   )
 }
+
 
 export default PracticePage
